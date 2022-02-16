83f421f57e1536c0c80ddc2317b8e56c8a869aa7
Feliks Weber <feliks.weber@tu-dresden.de>
2022-02-16 14:02:24 +0100

    Test the metadata header

--- src/test/test_opened_project.py
+++ src/test/test_opened_project.py
@@ -1,21 +1,32 @@
 from annotation_tool.opened_project_frame import AnnotationListModel, OpenedProjectFrame
 from annotation_tool.project import Project, Annotation
 
+annotation_data = {
+    "client_id": "id",
+    "path": "",
+    "text": "",
+    "up_votes": 0,
+    "down_votes": 0,
+    "age": "twenties",
+    "gender": "male",
+    "accent": "accent",
+    "modified": False,
+}
+
 def test_annotation_list():
     frame = OpenedProjectFrame()
     project = Project("")
-    data = {
-        "client_id": "0",
-        "path": "",
-        "text": "",
-        "up_votes": 0,
-        "down_votes": 0,
-        "age": "",
-        "gender": "",
-        "accent": "",
-        "modified": False,
-    }
-    project.annotations = [Annotation(data)] * 3
+    project.annotations = [Annotation(annotation_data)] * 3
     frame.load_project(project)
     model : AnnotationListModel = frame.annotationList.model()
     assert model.rowCount() == 3
+
+def test_metadata_header_filled_on_open():
+    frame = OpenedProjectFrame()
+    project = Project("")
+    project.annotations = [Annotation(annotation_data)] * 3
+    frame.load_project(project)
+    assert frame.accentEdit.text() == "accent"
+    assert frame.clientIdEdit.text() == "id"
+    assert frame.genderInput.currentText() == "Male"
+    assert frame.ageInput.currentText() == "19 - 29"
