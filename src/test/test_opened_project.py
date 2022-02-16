b1cba53d99f035e17c2d814870577cd21aae3f26
Feliks Weber <feliks.weber@tu-dresden.de>
2022-02-16 14:52:50 +0100

    Test shortcuts in tooltips

--- src/test/test_opened_project.py
+++ src/test/test_opened_project.py
@@ -1,3 +1,5 @@
+from PySide6.QtWidgets import QPushButton
+import pytest
 from annotation_tool.opened_project_frame import AnnotationListModel, OpenedProjectFrame
 from annotation_tool.project import Project, Annotation
 
@@ -13,20 +15,24 @@ annotation_data = {
     "modified": False,
 }
 
-def test_annotation_list():
+@pytest.fixture
+def project_frame():
     frame = OpenedProjectFrame()
     project = Project("")
     project.annotations = [Annotation(annotation_data)] * 3
     frame.load_project(project)
-    model : AnnotationListModel = frame.annotationList.model()
+    return frame
+
+def test_annotation_list(project_frame):
+    model : AnnotationListModel = project_frame.annotationList.model()
     assert model.rowCount() == 3
 
-def test_metadata_header_filled_on_open():
-    frame = OpenedProjectFrame()
-    project = Project("")
-    project.annotations = [Annotation(annotation_data)] * 3
-    frame.load_project(project)
-    assert frame.accentEdit.text() == "accent"
-    assert frame.clientIdEdit.text() == "id"
-    assert frame.genderInput.currentText() == "Male"
-    assert frame.ageInput.currentText() == "19 - 29"
+def test_metadata_header_filled_on_open(project_frame):
+    assert project_frame.accentEdit.text() == "accent"
+    assert project_frame.clientIdEdit.text() == "id"
+    assert project_frame.genderInput.currentText() == "Male"
+    assert project_frame.ageInput.currentText() == "19 - 29"
+
+def test_tooltips_have_shortcuts(project_frame):
+    play_button : QPushButton = project_frame.playPauseButton
+    assert play_button.shortcut().toString() in play_button.toolTip()
