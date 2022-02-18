3b972fb1186082f20910bb2e12c5d52c17fbee45
Feliks Weber <feliks.weber@tu-dresden.de>
2022-02-18 15:47:12 +0100

    Test playback icon loading

--- src/test/test_audio_playback_widget.py
+++ src/test/test_audio_playback_widget.py
@@ -6,3 +6,9 @@ def test_tooltips_have_shortcuts():
     widget = AudioPlaybackWidget()
     play_button : QPushButton = widget.playPauseButton
     assert play_button.shortcut().toString() in play_button.toolTip()
+
+def test_icons_loaded():
+    widget = AudioPlaybackWidget()
+    image = widget.playPauseButton.icon().pixmap(10,10).toImage()
+    value = image.pixelColor(3, 3).valueF()
+    assert value > 0.2 and value < 0.3
