c1690b0afc333a41b41d8013f834f520476c8cbb
Feliks Weber <feliks.weber@tu-dresden.de>
2022-02-21 08:42:12 +0100

    Test that removing annotations deletes files

--- test/test_opened_project.py
+++ test/test_opened_project.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from PySide6.QtWidgets import QPushButton
 import pytest
 from voice_annotation_tool.opened_project_frame import AnnotationListModel, OpenedProjectFrame
@@ -20,7 +22,9 @@ def project_frame():
     frame = OpenedProjectFrame()
     project = Project("")
     for annotation_num in range(3):
-        annotation_data["path"] = "/tmp/path_" + str(annotation_num)
+        path = Path("/tmp/path_" + str(annotation_num))
+        path.touch()
+        annotation_data["path"] = str(path)
         project.add_annotation(Annotation(annotation_data))
     frame.load_project(project)
     return frame
@@ -39,9 +43,14 @@ def test_tooltips_have_shortcuts(project_frame):
     play_button : QPushButton = project_frame.playPauseButton
     assert play_button.shortcut().toString() in play_button.toolTip()
 
-def test_delete_rows(project_frame: OpenedProjectFrame):
+def test_delete_removes_rows(project_frame: OpenedProjectFrame):
     model: AnnotationListModel = project_frame.annotationList.model()
     start = model.rowCount()
     selected = len(project_frame.get_selected_annotations())
     project_frame.delete_selected()
     assert model.rowCount() == start - selected
+
+def test_delete_removes_files(project_frame: OpenedProjectFrame):
+    selected: Annotation = project_frame.get_selected_annotations()[0]
+    project_frame.delete_selected()
+    assert not os.path.isfile(selected.path)
