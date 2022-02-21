8a926234fd6b68c66addcdf93cb24ff3d9ecd1e2
Feliks Weber <98736859+FeliksWeber@users.noreply.github.com>
2022-02-21 09:43:36 +0100

    Fix selection not updating when using shortcuts (#28)
    fixes #27

--- test/test_opened_project.py
+++ test/test_opened_project.py
@@ -43,6 +43,24 @@ def test_tooltips_have_shortcuts(project_frame):
     play_button : QPushButton = project_frame.playPauseButton
     assert play_button.shortcut().toString() in play_button.toolTip()
 
+def test_next_button_selects_one(project_frame: OpenedProjectFrame):
+    project_frame.next_pressed()
+    assert len(project_frame.annotationList.selectedIndexes()) == 1
+
+def test_next_button_selects_correct_annotation(project_frame: OpenedProjectFrame):
+    project_frame.next_pressed()
+    assert project_frame.annotationList.selectedIndexes()[0].row() == 1
+
+def test_previous_button_selects_one(project_frame: OpenedProjectFrame):
+    project_frame.next_pressed()
+    project_frame.previous_pressed()
+    assert len(project_frame.annotationList.selectedIndexes()) == 1
+
+def test_previous_button_selects_correct_annotation(project_frame: OpenedProjectFrame):
+    project_frame.next_pressed()
+    project_frame.previous_pressed()
+    assert project_frame.annotationList.selectedIndexes()[0].row() == 0
+
 def test_mark_unchanged(project_frame: OpenedProjectFrame):
     project_frame.mark_unchanged_pressed()
     project_frame.mark_unchanged_pressed()
