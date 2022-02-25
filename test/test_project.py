feaa697dd7a083ba32802031d35c5e7a2a0122d1
Feliks Weber <feliks.weber@tu-dresden.de>
2022-02-25 10:25:50 +0100

    Add type hints to project tests

--- test/test_project.py
+++ test/test_project.py
@@ -12,29 +12,29 @@ def project():
     return project
 
 
-def test_creation(project):
+def test_creation(project: Project):
     assert project
 
 
-def test_export_csv(project):
+def test_export_csv(project: Project):
     output = StringIO()
     project.exportCSV(output)
     output.seek(0)
     assert output.read() == "path;text\r\n"
 
 
-def test_export_json(project):
+def test_export_json(project: Project):
     output = StringIO()
     project.exportJson(output)
     output.seek(0)
     assert output.read() == '[{"path": "text"}]'
 
 
-def test_import_csv(project):
+def test_import_csv(project: Project):
     project.importCSV(StringIO("path;new"))
     assert project.annotations[0].sentence == "new"
 
 
-def test_import_json(project):
+def test_import_json(project: Project):
     project.importJson(StringIO('[{"path": "new"}]'))
     assert project.annotations[0].sentence == "new"
