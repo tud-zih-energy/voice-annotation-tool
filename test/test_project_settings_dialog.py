4ef5f83f92db19023f729df4e1ae3551dfd1c8eb
Feliks Weber <feliks.weber@tu-dresden.de>
2022-04-11 10:11:41 +0200

    Test project settings dialog

---
+++ test/test_project_settings_dialog.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from voice_annotation_tool.project import Project
+from voice_annotation_tool.project_settings_dialog import ProjectSettingsDialog
+
+
+def test_load_project_shows_paths():
+    dialog = ProjectSettingsDialog()
+    project = Project()
+    project.tsv_file = Path("tsv")
+    project.audio_folder = Path("audio")
+    dialog.load_project(project)
+    assert dialog.audioPathEdit.text() == "audio"
+    assert dialog.tsvPathEdit.text() == "tsv"
