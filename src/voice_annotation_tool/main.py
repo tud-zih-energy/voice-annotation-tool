be00c0fa00b677df3e0558bde5b4e52afff06b9d
Feliks Weber <feliks.weber@tu-dresden.de>
2022-06-17 09:54:21 +0200

    Fix Windows hotbar icon
    Fixes #66

--- src/voice_annotation_tool/main.py
+++ src/voice_annotation_tool/main.py
@@ -4,12 +4,19 @@ The entrypoint for the annotation tool.
 
 import sys
 
-from PySide6.QtCore import QLocale, QTranslator
+from PySide6.QtCore import QLocale, QSize, QTranslator
+from PySide6.QtGui import QIcon
 from voice_annotation_tool.application import Application
 
 
 def main():
     app = Application(sys.argv)
+    try:
+        import ctypes
+        myappid = u'mycompany.myproduct.subproduct.version' # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    except Exception:
+        pass
     # Load the translation here so slots have the right locale when using tr.
     translator = QTranslator()
     translator.load(QLocale(), "translations/")
