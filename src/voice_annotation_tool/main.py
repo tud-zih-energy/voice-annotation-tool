4f7b1a88b8e511e8b147e0168337e6654fad61a4
Feliks Weber <feliks.weber@tu-dresden.de>
2022-06-17 10:07:26 +0200

    Cleaner exception handling

--- src/voice_annotation_tool/main.py
+++ src/voice_annotation_tool/main.py
@@ -2,20 +2,20 @@
 The entrypoint for the annotation tool.
 """
 
+import ctypes
 import sys
 
-from PySide6.QtCore import QLocale, QSize, QTranslator
-from PySide6.QtGui import QIcon
+from PySide6.QtCore import QLocale, QTranslator
 from voice_annotation_tool.application import Application
 
 
 def main():
     app = Application(sys.argv)
     try:
-        import ctypes
-        myappid = u'mycompany.myproduct.subproduct.version' # arbitrary string
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
-    except Exception:
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            "TUDresden.VoiceAnnotationTool.Python.Release"
+        )
+    except AttributeError:
         pass
     # Load the translation here so slots have the right locale when using tr.
     translator = QTranslator()
